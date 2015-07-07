5053af0c5b466e28774fbc130f4ff127e562b814
Jason-Yuan <yuanbo0109@gmail.com>
2015-07-07 01:37:25 -0700

    add solutions for InsertionSort and SelectionSort

---
+++ Sorting Algorithm/Insertion Sort/InsertionSort.py
@@ -0,0 +1,30 @@
+##############################################################################################################################
+#      Project:                  Insertion Sort
+#      Time Complexity:          O(n2)  
+#      Space Complexity:         O(1)
+#      Stability:                Stable
+#      Info:                     Decrease-by-one sorting
+##############################################################################################################################
+
+# In-place insertion sort
+def Inplace_InsertionSort(L):
+	for i in range(1,len(L)):  # we assume L[0] is the sorted sublist
+		currentvalue = L[i]
+
+		while i > 0 and L[i-1] > currentvalue:
+			L[i]=L[i-1]
+			i -= 1
+
+		L[i] = currentvalue
+
+	return L
+
+
+##############################################################################################################################
+
+def main():
+	L = [1, 4, 3, 5, 6, 2]
+	print "Inplace InsertionSort: ", Inplace_InsertionSort(L)
+
+if __name__ == '__main__':
+	main()
