438e1f6e9352a55033c3e7c98df0008616ab2397
Jason-Yuan <yuanbo0109@gmail.com>
2015-07-09 17:42:36 -0700

    add more method for solving chapter 1.2 and 1.3

--- CTCI/Python/Chapter1-3.py
+++ CTCI/Python/Chapter1-3.py
@@ -1,6 +1,7 @@
 ##############################################################################################################################
+# Method 1
 # Ideas: For both of the them, for example, we can sort them with MergeSort, and then compare them.
-# Time Complexity: O(nlogn)
+# Time Complexity: O(nlogn) based on the sorting algorithm
 # Space Complexity: O(1)
 ##############################################################################################################################
 
@@ -31,7 +32,7 @@ def MergeSort(L):
 
 # end define merge sort
 
-def isPermutation(s1, s2):
+def IsPermutation1(s1, s2):
 	if len(s1) != len(s2):
 		return False
 	else:
@@ -44,12 +45,40 @@ def isPermutation(s1, s2):
 			return False
 
 ##############################################################################################################################
+# Method 2
+# Ideas: First create a flag array of 256 ZERO, since there are only 256 different ASCII characters.
+#        Then, for each of the char in string_1 map to the array, flag++
+#        And, for each of the char in string_2 map to the array, flag--
+#        If all elements in the array still 0, then string_2 is the permutation of string_1
+# Time Complexity: O(n) 
+# Space Complexity: O(1)
+##############################################################################################################################
+
+def IsPermutation2(s1, s2):
+	if len(s1) != len(s2):
+		return False
+	else:
+		Flag = [0 for i in range(256)]
+		for x in s1:
+			Flag[ord(x)] +=1
+
+		for x in s2:
+			Flag[ord(x)] -=1
+
+	for f in Flag:
+		if f != 0:
+			return False
+
+	return True
+
+##############################################################################################################################
 
 def main():
 	string1 = "Hello,World!"
 	string2 = "oHell,!Wordl"
 	print "Is \"{}\" a permutation of \"{}\"".format(string2, string1)
-	print "Result is: ", isPermutation(string1, string2)
+	print "Result 1: ", IsPermutation1(string1, string2)
+	print "Result 2: ", IsPermutation2(string1, string2)
 
 if __name__ == '__main__':
 	main()
