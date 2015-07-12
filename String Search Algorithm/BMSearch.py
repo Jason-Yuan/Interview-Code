c5c9eda498741b04f3bdeafe6a4f74b2ed3f4b2b
Jason-Yuan <yuanbo0109@gmail.com>
2015-07-11 18:20:16 -0700

    modified the BM algorithm

---
+++ String Search Algorithm/BMSearch.py
@@ -0,0 +1,10 @@
+##############################################################################################################################
+#      Project:                  Boyer Moore string Search Algorithm
+#      Time Complexity:          Best case: O(n/m)  /  Worst case: O(n*m)  
+#      Stability:                Not stable
+#      Info:                     BM algorithm use two lookup table for "Bad Character" and "Good Suffix" rules
+#                                BMH(Boyer-Moore-Horspool) algorithm only use "Bad Character" rule
+#                                Reference: 1. http://www.ruanyifeng.com/blog/2013/05/boyer-moore_string_search_algorithm.html
+#                                           2. https://www.youtube.com/watch?v=Wj606N0IAsw
+##############################################################################################################################
+
