836cc52c9a9181ef1ad17ac53affa790cbfbf099
Jason-Yuan <yuanbo0109@gmail.com>
2015-07-13 23:51:56 -0700

    add basic data structure python implementation

---
+++ Basic Data Structure/Stack.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+a = Stack()
+a.push(1)
+a.push(2)
+print a.pop()
