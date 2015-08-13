a428a9a1f6310bbd9e75a3bea2235707ad580668
Jason-Yuan <yuanbo0109@gmail.com>
2015-08-12 22:49:13 -0700

    add comments and solution ideas

--- CTCI/Python/Chapter5-1.py
+++ CTCI/Python/Chapter5-1.py
@@ -1,5 +1,18 @@
 ##############################################################################################################################
-# Ideas: 
+# Bit Manipulations:
+#                    >>    			# Right Shift
+# 					 <<    			# Left Shift
+# 					 &     			# Bitwise AND
+# 					 |     			# Bitwise OR
+# 					 ^     			# Bitwise XOR
+# 					 ~x    			# Bitwise NOT, returns complement of x which equals -x-1
+#                    bin(5)			# output 0b101
+#                    hex(15)        # output 0xf
+#					 int("111",2)   # output 7
+#					 int("0b111",2) # output 7
+# Ideas:     1. clean n from i to j, set to 0
+#            2. shift m i bits
+#            3. n_cleaned | m_shifted
 ##############################################################################################################################
  
 def updateBits(n, m, i, j):
@@ -10,11 +23,15 @@ def updateBits(n, m, i, j):
 	n_cleared = n & mask
 	m_shifted = m << i
 	return n_cleared | m_shifted
-	
+
 ##############################################################################################################################
 
 def main():
-	print '{0:b}'.format(updateBits(0b10000000000, 0b10011, 2, 6))
+	a = 0b10000000000
+	b = 0b10011
+	print "First number is: ", '{0:b}'.format(a)
+	print "Second number is: ", '{0:b}'.format(b)
+	print '{0:b}'.format(updateBits(a, b, 2, 6))
 
 if __name__ == '__main__':
 	main()
