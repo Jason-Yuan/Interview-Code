e7318343bc60c966c5d66dca702d30e26fbe07f6
Jason-Yuan <yuanbo0109@gmail.com>
2015-08-18 01:12:20 -0700

    add solution for follow up

--- CTCI/Python/Chapter9-2.py
+++ CTCI/Python/Chapter9-2.py
@@ -13,12 +13,31 @@ def findPath(m, n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m][n]
 
+# Follow up, with obstacles
+def findPathWithObs(m, n, obstacles = None):
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        temp1 = temp2 = 1
+        for j in range(n+1):
+            if (0, j) in obstacles:
+                temp1 = 0
+            dp[0][j] = temp1
+        for i in range(m+1):
+            if (i, 0) in obstacles:
+                temp2 = 0
+            dp[i][0] = temp2
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if (i, j) in obstacles:
+                    dp[i][j] = 0
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m][n]
+
 ##############################################################################################################################
 
 def main():
-	a = 3
-	b = 3
-	print "Number of unique path from (0, 0) to ({},{}) is {}".format(a, b, findPath(a, b))
+    a = [(1, 0), (1, 1)]
+    print "Number of unique path from (0, 0) to ({},{}) is {}".format(3, 3, findPath(3, 3))
+    print "Number of unique path from (0, 0) to ({},{}) with obstacles is {}".format(3, 3, findPathWithObs(3, 3, a))
 
 if __name__ == '__main__':
 	main()
