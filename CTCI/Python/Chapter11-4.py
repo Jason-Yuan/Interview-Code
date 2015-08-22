a3924fb6f7efb6188ca7e09c2b637338550bb649
Jason-Yuan <yuanbo0109@gmail.com>
2015-08-21 18:32:14 -0700

    add Chapter11.4 python solution

---
+++ CTCI/Python/Chapter11-4.py
@@ -0,0 +1,9 @@
+##############################################################################################################################
+# External Sort
+#
+# Ideas:	1. divide the file into many small pieces
+#       	2. sort piece by piece
+#           3. merge the small pieces, finally have a fully sorted file
+# Time Complexity: O(nlogn)
+# Space Complexity: limited
+##############################################################################################################################
