dbd6222d1e7191b6778e6c0d61272089b8cca322
Jason-Yuan <yuanbo0109@gmail.com>
2015-09-03 23:56:30 -0700

    first commit

---
+++ LeetCode/Unique_Binary_Search_Trees.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in xrange(1, n+1):
+            for j in xrange(i):
+                res[i] += res[j] * res[i-1-j]
+        return res[n]
