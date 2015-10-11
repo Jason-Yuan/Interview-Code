1713c7eb7c536385966c057b8f9ba0c5b4e09891
Jason-Yuan <yuanbo0109@gmail.com>
2015-10-11 15:17:15 -0700

    changed maxheapify to siftdown, changed buildMaxHeap to maxheapify, added siftup

--- Basic Data Structure/PriorityQueue.py
+++ Basic Data Structure/PriorityQueue.py
@@ -11,54 +11,59 @@ class PriorityQueue:
 		self.maxheap = [0]
 		self.currentSize = 0
 
-	def insert(self, data):
+	def put(self, data):
 		"""add a new item to the maxheap"""
 		self.maxheap.append(data)
 		self.currentSize += 1
-		#compare bottom-up
-		i = self.currentSize
-		# // means devide by 2 and return int
-		while i // 2 > 0:
-			if self.maxheap[i] > self.maxheap[i // 2]:
-				self.maxheap[i], self.maxheap[i // 2] = self.maxheap[i // 2], self.maxheap[i]
-			i = i // 2
+		self.siftUp(self.currentSize)
 
-	def findMax(self):
+	def peek(self):
 		"""returns the item with the maxmum key value"""
 		return self.maxheap[1]
 
-	def delMax(self):
+	def get(self):
 		"""returns the item with the maxmum key value, removing the item from the heap"""
 		res = self.maxheap[1]
 		self.maxheap[1] = self.maxheap[self.currentSize]
 		self.currentSize = self.currentSize - 1
 		self.maxheap.pop()
-		self.maxHeapify(self.maxheap, 1)
+		self.siftDown(1)
 		return res
 
-	def maxHeapify(self, L, index):
+	def siftUp(self, index):
+        # // means devide by 2 and return int
+        while index // 2 > 0:
+            if self.maxheap[index] < self.maxheap[index // 2]:
+                break
+            else:
+                self.maxheap[index], self.maxheap[index // 2] = self.maxheap[index // 2], self.maxheap[index] 
+            index = index // 2
+
+	def siftDown(self, index):
 		"""correct single violation in a sub-tree"""
 		if index > self.currentSize // 2:
 			return
 		# find the max child of current index
-		if index*2+1 > self.currentSize or L[index*2] > L[index*2+1]:
+		if index*2+1 > self.currentSize or self.maxheap[index*2] > self.maxheap[index*2+1]:
 			maxChild = index*2
 		else:
 			maxChild = index*2+1
-		if L[index] < L[maxChild]:
-			L[index], L[maxChild] = L[maxChild], L[index]
-		self.maxHeapify(L, index*2)
-		self.maxHeapify(L, index*2+1)
+		if self.maxheap[index] > self.maxheap[maxChild]:
+			return
+		else:
+			self.maxheap[index], self.maxheap[maxChild] = self.maxheap[maxChild], self.maxheap[index]
+		self.siftDown(index*2)
+		self.siftDown(index*2+1)
 
-	def buildMaxHeap(self, L):
+	def maxHeapify(self, L):
 		"""build a new maxheap from an unordered array"""
 		self.currentSize = len(L)
 		self.maxheap = [0] + L
 		for i in range(len(L)//2, 0, -1):
-			self.maxHeapify(self.maxheap, i)
+			self.siftDown(i)
 
-	def show(self):
-		print self.maxheap[1:]
+	def size(self):
+		return self.currentSize
 
 	def isEmpty(self):
 		return self.currentSize == 0
@@ -66,21 +71,21 @@ class PriorityQueue:
 ##############################################################################################################################
 
 def main():
-	MyPQ = PriorityQueue()
-	L = [6, 5, 7, 2, 1, 3, 8]
+    MyPQ = PriorityQueue()
+    L = [6, 5, 7, 2, 1, 3, 8]
 
-	MyPQ.buildMaxHeap(L)
-	print "Build a priority queue from a list L: ", L
-	MyPQ.show()
+    MyPQ.maxHeapify(L)
+    print "Build a priority queue from a list L: ", L
+    print "Size: ", MyPQ.size()
 
-	MyPQ.insert(15)
-	MyPQ.insert(11)
-	MyPQ.insert(31)
-	MyPQ.insert(24)
-	MyPQ.show()
+    MyPQ.put(15)
+    MyPQ.put(11)
+    MyPQ.put(31)
+    MyPQ.put(24)
 
-	print "Delete the max: ", MyPQ.delMax()
-	MyPQ.show()
+    print "Max is: ", MyPQ.peek()
+    print "Delete max: ", MyPQ.get()
+    print "Size: ", MyPQ.size()
 
 if __name__ == '__main__':
-	main()
+    main()
