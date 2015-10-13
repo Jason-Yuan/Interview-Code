41debb6de3c9bdd80484ab2433bde92c9355e653
Jason-Yuan <yuanbo0109@gmail.com>
2015-10-12 18:26:00 -0700

    first commit

---
+++ LeetCode/First_Bad_Version.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start, end = 0, n
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            if isBadVersion(mid):
+                end = mid
+            else:
+                start = mid
+        
+        if isBadVersion(start):
+            return start
+        else:
+            return end
