0b5d93e8b88aa3921df113bd2bf16906bca04876
Jason-Yuan <yuanbo0109@gmail.com>
2015-10-15 23:38:43 -0700

    first commit

---
+++ LeetCode/Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+    # convert the kth largest to smallest
+        return self.findKthSmallest(nums, len(nums)+1-k)
+
+    def findKthSmallest(self, nums, k):
+        if nums:
+            pos = self.partition(nums, 0, len(nums)-1)
+            if k > pos+1:
+                return self.findKthSmallest(nums[pos+1:], k-pos-1)
+            elif k < pos+1:
+                return self.findKthSmallest(nums[:pos], k)
+            else:
+                return nums[pos]
+
+    # choose the right-most element as pivot   
+    def partition(self, nums, l, r):
+        low = l
+        while l < r:
+            if nums[l] < nums[r]:
+                nums[l], nums[low] = nums[low], nums[l]
+                low += 1
+            l += 1
+        nums[low], nums[r] = nums[r], nums[low]
+        return low
