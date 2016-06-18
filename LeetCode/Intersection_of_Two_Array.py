82f74948562966995636f2c6420499f36334fd26
Jason-Yuan <yuanbo0109@gmail.com>
2016-06-18 01:44:10 -0700

    first commit

---
+++ LeetCode/Intersection_of_Two_Array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        map = {}
+        for x in nums1:
+            if x not in map:
+                map[x] = 1
+            
+        res = []
+        for y in nums2:
+            if y in map and map[y] == 1:
+                res.append(y)
+                map[y] -= 1
+            
+        return res
