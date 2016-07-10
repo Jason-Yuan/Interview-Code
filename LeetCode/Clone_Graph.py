de2516c87ce75616f88311c179a26d3df54a2ed5
Jason-Yuan <yuanbo0109@gmail.com>
2016-07-09 18:03:47 -0700

    add DFS method

--- LeetCode/Clone_Graph.py
+++ LeetCode/Clone_Graph.py
@@ -1,12 +1,15 @@
+# Method 1: BFS
 # Definition for a undirected graph node
 # class UndirectedGraphNode(object):
 #     def __init__(self, x):
 #         self.label = x
 #         self.neighbors = []
 
+import Queue
+
 class Solution(object):
     def __init__(self):
-        self.dict = {}
+        self.visited = {}
         
     def cloneGraph(self, node):
         """
@@ -16,22 +19,42 @@ class Solution(object):
         if not node:
             return None
             
-        nodes = []
-        # BFS clone node
-        nodes.append(node)
-        self.dict[node] = UndirectedGraphNode(node.label)
-        pointer = 0
-        while pointer < len(nodes):
-            for neighbor in nodes[pointer].neighbors:
-                if neighbor not in self.dict:
-                    nodes.append(neighbor)
-                    self.dict[neighbor] = UndirectedGraphNode(neighbor.label)
-            pointer += 1
+        newHead = UndirectedGraphNode(node.label)
+        self.visited[node] = newHead
+        
+        myQueue = Queue.Queue()
+        myQueue.put(node)
+        while myQueue.qsize():
+            current = myQueue.get()
+            for neighbor in current.neighbors:
+                if neighbor not in self.visited:
+                    newNode = UndirectedGraphNode(neighbor.label)
+                    self.visited[current].neighbors.append(newNode)
+                    self.visited[neighbor] = newNode
+                    myQueue.put(neighbor)
+                else: # turn directed graph to undirected graph
+                    self.visited[current].neighbors.append(self.visited[neighbor])
                     
-        # clone neighbors
-        for n in nodes:
-            newNode = self.dict[n]
-            for neighbor in n.neighbors:
-                newNode.neighbors.append(self.dict[neighbor])
-                
-        return self.dict[node]
+        return newHead
+
+# Method 2: DFS
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: UndirectedGraphNode
+        :rtype: UndirectedGraphNode
+        """
+        if not node:
+            return None
+            
+        return self.dfs(node, {})
+        
+    def dfs(self, node, map):
+        if node in map:
+            return map[node]
+        newNode = UndirectedGraphNode(node.label)
+        map[node] = newNode
+        for neighbor in node.neighbors:
+            newNode.neighbors.append(self.dfs(neighbor, map))
+            
+        return newNode
